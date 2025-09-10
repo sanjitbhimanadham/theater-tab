0a4bf8bd4bbde9b92fe315273f289f4bdc47488d
Sanjit Bhimanadham <sanjitbhimanadham@gmail.com>
2025-09-09 21:14:47 -0500

    add toggle between youtube and embed on click
    This is a crude implementation that allows users to switch back to YouTube. Timestamps are not preserved yet.

--- background.js
+++ background.js
@@ -1,12 +1,23 @@
 chrome.action.onClicked.addListener((tab) => {
-    if (tab.url && tab.url.includes('youtube.com/watch')) {
-        const url = new URL(tab.url);
-        const videoId = url.searchParams.get('v');
-    
+    if (!tab.url) return;
+
+    const url = new URL(tab.url);
+
+    if (url.hostname.includes("youtube.com") && url.pathname === "/watch") {
+        const videoId = url.searchParams.get("v");
         if (videoId) {
             const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-
             chrome.tabs.update(tab.id, { url: embedUrl });
         }
+        return;
+    }
+
+    if (url.hostname.includes("youtube.com") && url.pathname.startsWith("/embed/")) {
+        const videoId = url.pathname.split("/embed/")[1];
+        if (videoId) {
+            const watchUrl = `https://www.youtube.com/watch?v=${videoId}`;
+            chrome.tabs.update(tab.id, { url: watchUrl });
+        }
+        return;
     }
 });
